ce9c5d65da841ee20e414280edf619e17803f3a9
Stan <skukhniy@gmail.com>
2022-09-15 22:16:37 -0400

    error

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,15 +7,15 @@ function NavbarC() {
 	return (
 		<Navbar className="navbar-custom" variant="light" expand="xl">
 			<Container>
-				<Navbar.Brand href="#home">Stanislav Kukhniy </Navbar.Brand>
+				<Navbar.Brand>Stanislav Kukhniy </Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#link">About</Nav.Link>
+						<Nav.Link href="#link">About Me</Nav.Link>
+						<Nav.Link href="#home">Skills</Nav.Link>
 						<Nav.Link href="#link">Projects</Nav.Link>
-						<Nav.Link href="#link">Blog</Nav.Link>
 						<Nav.Link href="#link">Contact</Nav.Link>
+						<Nav.Link href="#link">Blog</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
