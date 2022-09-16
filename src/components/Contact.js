9d31a5c74643b104abb1921294ab4ca5372e718c
Stan <skukhniy@gmail.com>
2022-09-16 12:22:21 -0400

    added social media icons and email to COntact comp

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,41 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 export default function Contact() {
 	return (
 		<div className="contact-custom text-center p-3">
 			<h3>Contact</h3>
+			<h5>
+				Have a question or want to connect? Here are the places you can find me
+			</h5>
+			<div className="mb-3 mt-3">
+				<span>Email: </span>
+				<a href="mailto:skukhniy@gmail.com" style={{ color: "black" }}>
+					<span>skukhniy@gmail.com</span>
+				</a>
+			</div>
+			<div className="mb-3">
+				<SocialIcon
+					url="https://github.com/skukhniy"
+					fgColor="black"
+					bgColor="transparent"
+				/>
+				<SocialIcon
+					url="https://instagram.com/skukhniy"
+					fgColor="black"
+					bgColor="transparent"
+				/>
+				<SocialIcon
+					url="https://twitter.com/skukhniy"
+					fgColor="black"
+					bgColor="transparent"
+				/>
+				<SocialIcon
+					url="https://www.linkedin.com/in/stanislav-kukhniy-5b12a1138/"
+					fgColor="black"
+					bgColor="transparent"
+				/>
+			</div>
 		</div>
 	);
 }
