875c9e2ac337aa973ce554c1b4fa2aaedbb354ba
Stan <skukhniy@gmail.com>
2022-09-16 23:36:39 -0400

    Updates

---
+++ precache-manifest.58bed4e427c324d3261ebd59f91997b7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "761c7244e04781a73a71",
+    "url": "/personal-website/static/css/main.867c2280.chunk.css"
+  },
+  {
+    "revision": "761c7244e04781a73a71",
+    "url": "/personal-website/static/js/main.761c7244.chunk.js"
+  },
+  {
+    "revision": "512e96a52cc5fbf8a24f",
+    "url": "/personal-website/static/css/1.bdc9eda6.chunk.css"
+  },
+  {
+    "revision": "512e96a52cc5fbf8a24f",
+    "url": "/personal-website/static/js/1.512e96a5.chunk.js"
+  },
+  {
+    "revision": "7de3393924c49ef3b14b",
+    "url": "/personal-website/static/js/runtime~main.7de33939.js"
+  },
+  {
+    "revision": "e56e433c672a3e68785e94efdf45204a",
+    "url": "/personal-website/static/media/Me.e56e433c.png"
+  },
+  {
+    "revision": "560bc9428f8a1608510a39c2328ba592",
+    "url": "/personal-website/static/media/shop-screenshot.560bc942.png"
+  },
+  {
+    "revision": "42faa7a67507691763b7120102a8d796",
+    "url": "/personal-website/static/media/waldo-screenshot.42faa7a6.png"
+  },
+  {
+    "revision": "4cf04b1733104b94e08a1fef948daa50",
+    "url": "/personal-website/static/media/starwars-screenshot.4cf04b17.png"
+  },
+  {
+    "revision": "f1437c0f068dc65f7e55506b453ab3ab",
+    "url": "/personal-website/static/media/pokedex-screenshot.f1437c0f.png"
+  },
+  {
+    "revision": "e99d7eb905a8635fae66507ea584880c",
+    "url": "/personal-website/index.html"
+  }
+];
